189f9dfd04df0a57b54103903b37f3d15133bce3
Raphaël Taïeb <raphael.taieb.fr@gmail.com>
2026-05-30 14:14:52 +0200

    Fix word accents and remove duplicates

--- src/pseudos.ts
+++ src/pseudos.ts
@@ -4,7 +4,7 @@ export const PSEUDOS = [
   "Félicien", "Francine", "Jean-Bernard", "Marie-Huguette", "Jean-Michel", "Marie-Chantal", "Jean-Eustache", "Marie-Thérèse",
   "Jean-Claude", "Marie-Antoinette", "Jean-Jacques", "Marie-Paule", "Jean-Pierre", "Marie-Louise", "Jean-Luc", "Marie-Madeleine",
   "Jean-Marc", "Marie-Christine", "Jean-Paul", "Marie-France", "Jean-François", "Marie-José", "Jean-Louis", "Marie-Claire",
-  "Jean-Marie", "Marie-Ange", "Jean-René", "Marie-Claude", "Jean-Baptiste", "Marie-Jeanne", "Jean-Philippe", "Marie-Laure",
+  "Jean-Marie", "Marie-Ange", "Jean-René", "Marie-Claude", "Jean-Eudes", "Marie-Jeanne", "Jean-Philippe", "Marie-Laure",
   "Jean-Charles", "Marie-Pierre", "Jean-Christophe", "Marie-Odile", "Jean-Dominique", "Marie-Hélène", "Jean-Pascal", "Marie-Agnès",
   "Bébert", "Mireille", "Dédé", "Georgette", "Jojo", "Suzanne", "Pierrot", "Yvonne", "René", "Simone",
   "Robert", "Jeannine", "Maurice", "Monique", "André", "Colette", "Albert", "Gisèle", "Roger", "Denise",
